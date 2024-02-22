20e12344116aed357e3bba323a7eb384b1132404
hundodayso <solder-elision.0y@icloud.com>
2024-02-22 22:35:31 +0000

    1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 ##Test Change
+number = [1, 2, 3]
+new_numbers = [n + 1 for n in number]
+name = "Angela"
+new_list = [letter for letter in name]
+range_change = [n + 1 for n in range(1,5)]
+range_change2 = [n * 2 for n in range(1,5)]
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+long_names_test = [name.upper() for name in names]
+long_names = [name.upper() for name in names if len(name) > 4]
